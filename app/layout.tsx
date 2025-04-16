5a5f6a49cfe8a4ba737f9a0e7e3597d5679cbd46
Agung Fathul Muhtadin <90741091+AgungFathul01@users.noreply.github.com>
2025-04-16 08:22:55 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'RedesignSMKDEV',
+  description: 'RedesignSMKDEV',
+  generator: 'RedesignSMKDEV',
 }
 
 export default function RootLayout({
